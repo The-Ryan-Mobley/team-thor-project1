061a91c0c6c2ec05cf7448b4b4dc35e449bcd5d8
Ryan Mobley <ryan.mobley94@gmail.com>
2019-08-03 11:49:14 -0500

    added leaflet map

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 //https://api.nasa.gov/planetary/apod?api_key=nUEptcB91ocekFHCzpFrc6dnWCcnRhIVaNgEVOTd
 //https://ssd-api.jpl.nasa.gov/nhats.api
 //https://www.sky-map.org/?ra=18.5&de=40&zoom=3
+//http://server1.sky-map.org/skywindow.jsp?object=M100&zoom=8&img_source=SDSS
 // $.ajax({
 //     url: "https://ssd-api.jpl.nasa.gov/nhats.api",
 //     method: "GET",
@@ -17,11 +18,13 @@
 //     console.log(response);
 
 // });
-$.ajax({
-    url: "https://www.sky-map.org/?ra=18.5&de=40&zoom=3",
-    method: "GET",
-
-}).then((response)=>{
-    console.log(response);
-
+var map = L.map('mapid', {
+    center: [51.505, -0.09],
+    zoom: 13
 });
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibWVhdHNoaWVsZG1hbiIsImEiOiJjanl2cnRvanAwZXVkM2NvYm16MzRzdXB4In0.c0UspdiYTGPjlbOdWti3ww'
+}).addTo(map);
