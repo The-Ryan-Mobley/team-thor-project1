3f332b692bc3756d2aebb9b73ef4774661021b39
Ryan Mobley <ryan.mobley94@gmail.com>
2019-08-06 18:06:53 -0500

    working on openweather before class, conversion functions and api call on mapclick function

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,14 +12,7 @@
 // });
 //http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
 var weatherKey="40d4a57683aeb7e88b7acf955c82d2a6";
-$.ajax({
-    url: "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID="+ weatherKey,
-    method: "GET",
 
-}).then((response)=>{
-    console.log(response);
-
-});
 var map = L.map('map', {
     center: [29.7602, -95.3694],
     zoom: 13
@@ -34,6 +27,44 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 var popup = L.popup();
 var latitude;
 var longitude;
+
+function getForecast(lat, lon){
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&APPID="+ weatherKey,
+        method: "GET",
+    
+    }).then((response)=>{
+        console.log(response);
+        //response.list[0] for todays weather
+
+    
+    });
+}
+function displayForecast(clouds, wind, temp,rain){
+    tempInF = 9/5(temp-237)+32;
+    temoInC = temp - 237;
+    let weatherContainer = $('<div class="weather-div">');
+    let cloudDOM = $('<div class="weather-cell">');
+    let windDOM = $('<div class="weather-cell"');
+    let tempDOM = $('<div class="weather-cell">');
+    let rainDOM = $('<div class="weather-cell">');
+
+}
+function findWindDirection(windDeg){
+    let directions=['N','S','E','W','NE','NW','SE','SW'];
+    let windDirection = "";
+    if((windDeg >= 340) || (windDeg <= 30)){
+        windDirection = directions[0];
+    }
+    else if((windDeg > 30)&&(windDeg <= 60)){
+        windDirection = directions[4];
+    }
+    else if((windDeg > 60)&&(windDeg <= 60))
+    return windDirection;
+    
+}
+
+
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
@@ -42,6 +73,9 @@ function onMapClick(e) {
         console.log(e.latlng);
         latitude=e.latlng.lat;
         longitude=e.latlng.lng;
+        getForecast(latitude.toString(),longitude.toString());
 }
 
 map.on('click', onMapClick);
+
+
