f6b41764821d0f82859796d8ca16796c06232197
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-08-06 21:12:57 -0500

    changed star-image so that it resizes with the document size, for responsiveness

--- assets/js/app.js
+++ assets/js/app.js
@@ -103,7 +103,9 @@ function findWindDirection(windDeg){
 function getskyImage(long, time){
     let skyhours = long/15;
     let skyqueryurl = "http://server1.sky-map.org/skywindow?ra="+skyhours+" 00 00&de="+long+" 00 00&zoom=4";
-    let skyimg = $("<IFRAME SRC='"+skyqueryurl+" WIDTH=400 HEIGHT=320' WIDTH=400 HEIGHT=320>    </IFRAME>");
+    let imgwidth = $("#sky-images").width();
+    console.log(imgwidth);
+    let skyimg = $("<IFRAME SRC='"+skyqueryurl+" WIDTH="+imgwidth+" HEIGHT="+(imgwidth*4)/5+"' WIDTH="+imgwidth+" HEIGHT="+(imgwidth*4)/5+">    </IFRAME>");
     $("#sky-images").empty();
     $("#sky-images").append(skyimg);
 }
